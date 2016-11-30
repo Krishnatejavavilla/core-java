f48af3a8e047dcda2a62cbb0beb3de83bc51f78b
Krishnatejavavilla <vavilla.krishnateja777@gmail.com>
2016-11-30 19:12:03 +0530

    Add files via upload

---
+++ Forloop5.java
@@ -0,0 +1,30 @@
+package forloop5;
+
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i=1;i<=7;i++)
+{
+	for (int j=0;j<i;j++)
+	{
+		System.out.print("*");
+		}
+		
+		System.out.println("");
+	}
+for (int i=7;i>=0;i--)
+{
+	for (int j=0;j<i;j++)
+	{
+		System.out.print("*");
+		}
+		
+		System.out.println("");
+	}
+
+
+}
+}
+
