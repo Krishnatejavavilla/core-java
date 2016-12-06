2ae81a9253f4c2572295b9b439701629b04afb5f
Krishnatejavavilla <vavilla.krishnateja777@gmail.com>
2016-12-06 17:07:55 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,25 @@
+package array;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int b[]=new int[12];
+		int i;
+		b[0]=3;
+		b[1]=7;
+		b[2]=9;
+		b[3]=18;
+		b[4]=25;
+		b[5]=30;
+		b[6]=72;
+		b[7]=213;
+		b[8]=351;
+		b[9]=430;
+		b[10]=472;
+		for(i=0;i<=10;i++)
+			System.out.println(b[i]);
+
+	}
+
+}
