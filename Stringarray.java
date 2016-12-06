dc560ff6e813c92a8a9ebb05d9c9acaed047e524
Krishnatejavavilla <vavilla.krishnateja777@gmail.com>
2016-12-06 17:15:23 +0530

    Add files via upload

---
+++ Stringarray.java
@@ -0,0 +1,20 @@
+package stringarray;
+
+public class Stringarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String  d[]=new String[8];
+		d[0]="Time";
+		d[1]="to";
+		d[2]="start";
+		d[3]="my";
+		d[4]="Good";
+		d[5]="career";
+		d[6]="very";
+		d[7]="quickly";
+		for(int i=0;i<=7;i++)
+			System.out.println(d[i]);
+	}
+
+}
